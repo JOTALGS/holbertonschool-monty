ec48105c193f48709b8e86c76fc64556d68d5f27
juandi131 <juandigarcia3@gmail.com>
2023-09-05 11:20:45 -0700

    12

--- pint.c
+++ pint.c
@@ -1,23 +1,32 @@
 #include "monty.h"
 
-void
-pint_st(stack_t **st, unsigned int line)
-{
-	stack_t *ptr;
-	(void)line;
 
-	ptr = *st;
-	if (ptr)
+void freeList(stack_t *stack)
+{
+	if (stack != NULL)
 	{
-		while (ptr->next)
-		{
-			ptr = ptr->next;
-		}
-		printf("%d\n", ptr->n);
+		if (stack->next != NULL)
+			freeList(stack->next);
+		free(stack);
 	}
-	else
+}
+/**
+ * pint_st - prits the value at the top of the stack
+ * @st: the stack
+ * @line: the line of the command
+*/
+void pint_st(stack_t **st, unsigned int line)
+{
+	stack_t *aux;
+
+	if (*st == NULL)
 	{
+		freeList(*st);
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line);
 		exit(EXIT_FAILURE);
 	}
+	aux = *st;
+	while (aux->next != NULL)
+		aux = aux->next;
+	printf("%d\n", aux->n);
 }
